5d53d857613f63cc6bbabd986404f732423e8483
Szymon Feliks <sz.feliks@gmail.com>
2017-08-20 18:48:07 +0200

    solved project

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,45 @@
 $(function() {
 
-  // your code will go here
+  // step 1, add Report Card Ajax request
+    $.ajax({
+    url: 'https://www.codeschool.com/users/sfeliks2016.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      //console.log('response', response.courses.completed)
+        
+        addCourses(response.courses.completed);
+    }
+  });
+    
+    // step 2, Add DOM Elements
+    
+    function addCourses(courses) {
+        
+        var $badges = $('#badges');
+        
+        courses.forEach(function(courses) {
+            
+            var $course = $('<div />', {
+                'class': 'course'
+            }).appendTo($badges);
+            
+            $('<h3 />', {
+              text: courses.title
+            }).appendTo($course)
+            
+            $('<img />', {
+                src: courses.badge
+            }).appendTo($course);
+            
+            $('<a />', {
+              'class': 'btn btn-primary',
+              target: '_blank',
+              href: courses.url,
+              text: 'See Course'
+            }).appendTo($course);
+            
+        })
+        
+    }
 
 });
